3fc78d9e48108481ddbe4824a8fc24df4af124e0
David Jackson <david@learnspree.com>
2018-02-18 21:47:16 +0000

    Adding DynamoDB code for metrics service - incomplete and untested

--- lambda-metrics-service/Handler.cs
+++ lambda-metrics-service/Handler.cs
@@ -1,5 +1,8 @@
 using Amazon.Lambda.Core;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using System;
+using System.Collections.Generic;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -12,6 +15,49 @@ namespace ServerlessPerformanceFramework
            // default to successful response until we plug in DynamoDB integration
            return new AddMetricsResponse("Lambda Metrics data persisted in DynamoDB successfully.", request, 0);
        }
+
+       private void CreateItem(AddMetricsRequest metrics)
+       {
+            // TODO - remove hardocded values and take from metrics object
+
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+            var request = new PutItemRequest
+            {
+                TableName = "ServerlessFunctionMetrics",
+                Item = new Dictionary<string, AttributeValue>()
+            {
+                { "FunctionName", new AttributeValue {
+                      S = "my-service"
+                  }},
+                { "Timestamp", new AttributeValue {
+                      S = "0000000000"
+                  }},
+                { "FunctionVersion", new AttributeValue {
+                      S = "$LATEST"
+                  }},
+                { "Duration", new AttributeValue {
+                      N = "20.00"
+                  }},
+                { "BilledDuration", new AttributeValue {
+                      N = "30.00"
+                  }},
+                { "MemorySize", new AttributeValue {
+                      N = "30.00"
+                  }},
+                { "MemoryUsed", new AttributeValue {
+                      N = "30.00"
+                  }},
+                { "LanguageRuntime", new AttributeValue {
+                      S = "Java"
+                  }},
+                { "ServerlessPlatformName", new AttributeValue {
+                      S = "AWS"
+                  }}
+            }
+            };
+            client.PutItem(request);
+        }
     }
 
     public class AddMetricsResponse
