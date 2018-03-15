a23c7939d80ec4110ae387986eb5b76ca9dcaf43
David Jackson <david@learnspree.com>
2018-03-15 20:53:59 +0000

    speeding up rule scripts by running in background

--- bin/disable-all-warmstart-rules.sh
+++ bin/disable-all-warmstart-rules.sh
@@ -1,5 +1,5 @@
-aws events disable-rule --name warmstart-node610-minute
-aws events disable-rule --name warmstart-java8-minute
-aws events disable-rule --name warmstart-dotnet2-minute
-aws events disable-rule --name warmstart-python3-minute
-aws events disable-rule --name warmstart-golang-minute
+aws events disable-rule --name warmstart-node610-minute &
+aws events disable-rule --name warmstart-java8-minute &
+aws events disable-rule --name warmstart-dotnet2-minute &
+aws events disable-rule --name warmstart-python3-minute &
+aws events disable-rule --name warmstart-golang-minute &
--- bin/enable-all-coldstart-rules.sh
+++ bin/enable-all-coldstart-rules.sh
@@ -1,5 +1,5 @@
-aws events enable-rule --name coldstart-node610-hourly
-aws events enable-rule --name coldstart-java8-hourly
-aws events enable-rule --name coldstart-dotnet2-hourly
-aws events enable-rule --name coldstart-python3-hourly
-aws events enable-rule --name coldstart-golang-hourly
+aws events enable-rule --name coldstart-node610-hourly &
+aws events enable-rule --name coldstart-java8-hourly &
+aws events enable-rule --name coldstart-dotnet2-hourly &
+aws events enable-rule --name coldstart-python3-hourly &
+aws events enable-rule --name coldstart-golang-hourly &
