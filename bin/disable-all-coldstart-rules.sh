0298f7fe4e4456bc69d691558b83c3774c921e28
David Jackson <david@learnspree.com>
2019-02-18 22:09:50 +0000

    Changed disable-all-coldstart-rules.sh to take optional parameter

--- bin/disable-all-coldstart-rules.sh
+++ bin/disable-all-coldstart-rules.sh
@@ -1,6 +1,19 @@
-aws events disable-rule --name coldstart-node610-hourly --profile serverless &
-aws events disable-rule --name coldstart-node810-hourly --profile serverless &
-aws events disable-rule --name coldstart-java8-hourly --profile serverless &
-aws events disable-rule --name coldstart-dotnet2-hourly --profile serverless &
-aws events disable-rule --name coldstart-python3-hourly --profile serverless &
-aws events disable-rule --name coldstart-golang-hourly --profile serverless &
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied - using default aws cli profile"
+    aws events disable-rule --name coldstart-node610-hourly 
+    aws events disable-rule --name coldstart-node810-hourly
+    aws events disable-rule --name coldstart-java8-hourly
+    aws events disable-rule --name coldstart-dotnet2-hourly
+    aws events disable-rule --name coldstart-python3-hourly
+    aws events disable-rule --name coldstart-golang-hourly
+  else
+    echo "Using aws cli profile '$1'"
+    aws events disable-rule --name coldstart-node610-hourly --profile $1 &
+    aws events disable-rule --name coldstart-node810-hourly --profile $1 &
+    aws events disable-rule --name coldstart-java8-hourly --profile $1 &
+    aws events disable-rule --name coldstart-dotnet2-hourly --profile $1 &
+    aws events disable-rule --name coldstart-python3-hourly --profile $1 &
+    aws events disable-rule --name coldstart-golang-hourly --profile $1 &
+fi
+
