8de52a921b4ded8e4a23d065460cb96a4feb3a40
David Jackson <david@learnspree.com>
2019-05-11 21:25:18 +0100

    Got scan working - needs cleanup and change to query

--- spf-api/getruntime/handler.py
+++ spf-api/getruntime/handler.py
@@ -4,13 +4,19 @@ import json
 
 from getruntime import decimalencoder
 import boto3
-dynamodb = boto3.resource('dynamodb')
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError, ParamValidationError
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 def getMaximum(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    #table = dynamodb.Table('TestServerless')
 
     # fetch metrics from the database
-    print('Language Runtime: ', event['pathParameters']['runtimeId'])
+    inputRuntime = '{}'.format(event['pathParameters']['runtimeId'])
+    print('Language Runtime: ', inputRuntime)
+    
 
     # TODO - change to a query as get_item requires full key including ID ?
     # START RequestId: 05a3e6e4-c39d-4064-a798-9df5b5a47377 Version: $LATEST
@@ -23,13 +29,42 @@ def getMaximum(event, context):
     #         'LanguageRuntime': event['pathParameters']['runtimeId']
     #     }
     # )
+    
+    try:
+        # result = table.query(
+        #     KeyConditionExpression=Key('LanguageRuntime').eq('{}'.format(inputRuntime)),
+        #     ProjectionExpression='LanguageRuntime'
+        # )
+        result = table.scan()
+    except ParamValidationError as e:
+        print("Parameter validation error: %s" % e)        
+    except ClientError as e:
+        print("Unexpected error: %s" % e)
+    except Exception as e:
+        print("Generic error: %s" % e)
+        
+    
+    # result = dynamodb.query(
+    #     TableName='TestServerless',
+    #     KeyConditionExpression=Key('LanguageRuntime').eq('{}'.format(inputRuntime)) 
+    # )
+
+    print('Language Runtime: ', inputRuntime)
+
+    returnValue = ""
+    print(result)
+    for i in result['Items']:
+        newValue = i['LanguageRuntime'], ":" #, i['RequestId']
+        print(newValue)
+        returnValue += newValue
 
     # create a response
     response = {
         "statusCode": 200,
+        "body": returnValue
         #"body": json.dumps(result['Item'],
-                           # cls=decimalencoder.DecimalEncoder)
-        "body": "completed"
+                          # cls=decimalencoder.DecimalEncoder)
+        # "body": "completed"
     }
 
     return response
