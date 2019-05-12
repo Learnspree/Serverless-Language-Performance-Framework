8d2ce12a1bb73ab6b2b2358157206d5c72d36668
David Jackson <david@learnspree.com>
2019-05-12 20:33:29 +0100

    Updated code to look for maximum value returned from query - need to fix reserved keyword issue on Duration column

--- spf-api/getruntime/handler.py
+++ spf-api/getruntime/handler.py
@@ -16,24 +16,11 @@ def getMaximum(event, context):
     inputRuntime = '{}'.format(event['pathParameters']['runtimeId'])
     print('Language Runtime Input: ', inputRuntime)
     
-
-    # TODO - change to a query as get_item requires full key including ID ?
-    # START RequestId: 05a3e6e4-c39d-4064-a798-9df5b5a47377 Version: $LATEST
-    # Language Runtime:  java8
-    #     raise error_class(parsed_response, operation_name) _make_api_calliong the GetItem operation: The provided key element does not match the schema
-    # END RequestId: 05a3e6e4-c39d-4064-a798-9df5b5a47377
-    # REPORT RequestId: 05a3e6e4-c39d-4064-a798-9df5b5a47377	Duration: 287.08 ms	Billed Duration: 300 ms 	Memory Size: 128 MB	Max Memory Used: 67 MB	
-    # result = table.get_item(
-    #     Key={
-    #         'LanguageRuntime': event['pathParameters']['runtimeId']
-    #     }
-    # )
-    
     try:
         result = table.query(
             TableName=os.environ['DYNAMODB_TABLE'],
             KeyConditionExpression=Key('LanguageRuntime').eq('{}'.format(inputRuntime)),
-            ProjectionExpression='LanguageRuntime'
+            ProjectionExpression='LanguageRuntime, Duration'
         )
         # result = table.scan()
     except ParamValidationError as e:
@@ -43,28 +30,19 @@ def getMaximum(event, context):
     except Exception as e:
         print("Generic error: %s" % e)
         
-    
-    # result = dynamodb.query(
-    #     TableName='TestServerless',
-    #     KeyConditionExpression=Key('LanguageRuntime').eq('{}'.format(inputRuntime)) 
-    # )
-
-    print('Language Runtime: ', inputRuntime)
-
     returnValue = ""
+    maxDuration = -1
     print(result)
     for i in result['Items']:
-        newValue = i['LanguageRuntime'], ":" #, i['RequestId']
-        print(newValue)
-        returnValue += newValue
-
+        duration = i['Duration']
+        if duration > maxDuration:
+            maxDuration = duration
+            returnValue = json.dumps(i) 
+    
     # create a response
     response = {
         "statusCode": 200,
         "body": returnValue
-        #"body": json.dumps(result['Item'],
-                          # cls=decimalencoder.DecimalEncoder)
-        # "body": "completed"
     }
 
     return response
