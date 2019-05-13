292661af71675afb78d8acec62a75c98ae14ac9b
David Jackson <david@learnspree.com>
2019-05-13 21:46:54 +0100

    Fixed decimal conversion of dynamodb response

--- spf-api/getruntime/decimalencoder.py
+++ spf-api/getruntime/decimalencoder.py
@@ -4,5 +4,5 @@ import json
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):
-            return int(obj)
+            return str(obj)
         return super(DecimalEncoder, self).default(obj)
