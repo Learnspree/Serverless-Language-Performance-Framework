31e37b9dbe4c4c3628426d809d77ef249f7b56cf
David Jackson <davidjacksonirl@gmail.com>
2019-11-07 18:52:01 +0100

    added basic cold/warm start detection to java8 test function

--- aws-test/aws-service-java8/src/main/java/com/learnspree/EmptyJava8Handler.java
+++ aws-test/aws-service-java8/src/main/java/com/learnspree/EmptyJava8Handler.java
@@ -6,8 +6,11 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class EmptyJava8Handler implements RequestHandler<Map<String,Object>, Response>  {
 
+	private static int INVOKE_COUNT = 0;
+
 	@Override
 	public Response handleRequest(Map<String,Object> input, Context context) {
+		System.out.println("State: " + ((INVOKE_COUNT++ > 0) ? "Warm" : "Cold"));
 		return new Response("Empty Java 8 Test Function Completed.");
 	}
 }
