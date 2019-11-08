46c59215306a147b8f7173998c236c42d77bd27b
David Jackson <davidjacksonirl@gmail.com>
2019-11-08 19:07:25 +0100

    removed static from java test function - not needed anymore

--- aws-test/aws-service-java8/src/main/java/com/learnspree/EmptyJava8Handler.java
+++ aws-test/aws-service-java8/src/main/java/com/learnspree/EmptyJava8Handler.java
@@ -6,11 +6,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class EmptyJava8Handler implements RequestHandler<Map<String,Object>, Response>  {
 
-	private static int INVOKE_COUNT = 0;
-
 	@Override
 	public Response handleRequest(Map<String,Object> input, Context context) {
-		System.out.println(String.format("RequestId: %s State: %s", context.getAwsRequestId(), ((INVOKE_COUNT++ > 0) ? "Warm" : "Cold")));
 		return new Response("Empty Java 8 Test Function Completed.");
 	}
 }
