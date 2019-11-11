eb1c37e0d5aaa244526a5852f3e2cfbfc55fff7f
David Jackson <davidjacksonirl@gmail.com>
2019-11-11 13:13:29 +0000

    Added initial version of function to enable bursty calls for cold-start testing. Doesn't quite work yet - need to add permissions for lambda invocation

---
+++ aws-test/aws-burst-invoker/handler.py
@@ -0,0 +1,20 @@
+from boto3 import client as boto3_client
+from datetime import datetime
+import json
+
+lambda_client = boto3_client('lambda')
+
+def burst_invoker(event, context):
+
+    print('Event Count: ', event['invokeCount'])
+    print('Event Target: ', event['targetFunctionName'])
+
+    # putting in a hard limit for safety
+    if event['invokeCount'] > 100:
+        return
+
+    for x in range(event['invokeCount']):
+        invoke_response = lambda_client.invoke(FunctionName=event['targetFunctionName'],
+                                            InvocationType='Event',
+                                            Payload=None)
+        print(invoke_response)
