d0c5b67d4979375f302204c225a496dc4b9bf139
David Jackson <davidjacksonirl@gmail.com>
2019-11-19 22:03:48 +0000

    fix typo in enable-cold-start-rules script

--- bin/enable-all-coldstart-rules.sh
+++ bin/enable-all-coldstart-rules.sh
@@ -1,16 +1,16 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied - using default aws cli profile"
-    aws events disable-rule --name coldstart-python36-hourly-burst
-    aws events disable-rule --name coldstart-node810-hourly-burst
-    aws events disable-rule --name coldstart-java8-hourly-burst
-    aws events disable-rule --name coldstart-go-hourly-burst
-    aws events disable-rule --name coldstart-dotnet2-hourly-burst
+    aws events enable-rule --name coldstart-python36-hourly-burst
+    aws events enable-rule --name coldstart-node810-hourly-burst
+    aws events enable-rule --name coldstart-java8-hourly-burst
+    aws events enable-rule --name coldstart-go-hourly-burst
+    aws events enable-rule --name coldstart-dotnet2-hourly-burst
   else
     echo "Using aws cli profile '$1'"
-    aws events disable-rule --name coldstart-python36-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-node810-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-java8-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-go-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-dotnet2-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-python36-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-node810-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-java8-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-go-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-dotnet2-hourly-burst --profile $1 &
 fi
