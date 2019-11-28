15e7de3866d5fc70e101eba382d550f49fa77bc6
David Jackson <davidjacksonirl@gmail.com>
2019-11-28 21:00:22 +0000

    fixed dev stage start/stop rules scripts

--- bin/disable-all-rules.sh
+++ bin/disable-all-rules.sh
@@ -1,26 +1,26 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied - using default aws cli profile"
-    aws events disable-rule --name warmstart-node810-minute 
-    aws events disable-rule --name warmstart-java8-minute 
-    aws events disable-rule --name warmstart-dotnet2-minute
-    aws events disable-rule --name warmstart-python3-minute
-    aws events disable-rule --name warmstart-golang-minute
-    aws events disable-rule --name coldstart-python36-hourly-burst
-    aws events disable-rule --name coldstart-node810-hourly-burst
-    aws events disable-rule --name coldstart-java8-hourly-burst
-    aws events disable-rule --name coldstart-go-hourly-burst
-    aws events disable-rule --name coldstart-dotnet2-hourly-burst
+    aws events disable-rule --name warmstart-node810-dev-minute 
+    aws events disable-rule --name warmstart-java8-dev-minute 
+    aws events disable-rule --name warmstart-dotnet2-dev-minute
+    aws events disable-rule --name warmstart-python3-dev-minute
+    aws events disable-rule --name warmstart-golang-dev-minute
+    aws events disable-rule --name coldstart-python36-dev-hourly-burst
+    aws events disable-rule --name coldstart-node810-dev-hourly-burst
+    aws events disable-rule --name coldstart-java8-dev-hourly-burst
+    aws events disable-rule --name coldstart-go-dev-hourly-burst
+    aws events disable-rule --name coldstart-dotnet2-dev-hourly-burst
   else
     echo "Using aws cli profile '$1'"
-    aws events disable-rule --name warmstart-node810-minute --profile $1 &
-    aws events disable-rule --name warmstart-java8-minute --profile $1 &
-    aws events disable-rule --name warmstart-dotnet2-minute --profile $1 &
-    aws events disable-rule --name warmstart-python3-minute --profile $1 &
-    aws events disable-rule --name warmstart-golang-minute --profile $1 &
-    aws events disable-rule --name coldstart-python36-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-node810-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-java8-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-go-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-dotnet2-hourly-burst --profile $1 &
+    aws events disable-rule --name warmstart-node810-dev-minute --profile $1 &
+    aws events disable-rule --name warmstart-java8-dev-minute --profile $1 &
+    aws events disable-rule --name warmstart-dotnet2-dev-minute --profile $1 &
+    aws events disable-rule --name warmstart-python3-dev-minute --profile $1 &
+    aws events disable-rule --name warmstart-golang-dev-minute --profile $1 &
+    aws events disable-rule --name coldstart-python36-dev-hourly-burst --profile $1 &
+    aws events disable-rule --name coldstart-node810-dev-hourly-burst --profile $1 &
+    aws events disable-rule --name coldstart-java8-dev-hourly-burst --profile $1 &
+    aws events disable-rule --name coldstart-go-dev-hourly-burst --profile $1 &
+    aws events disable-rule --name coldstart-dotnet2-dev-hourly-burst --profile $1 &
 fi
--- bin/enable-all-coldstart-rules.sh
+++ bin/enable-all-coldstart-rules.sh
@@ -1,16 +1,16 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied - using default aws cli profile"
-    aws events enable-rule --name coldstart-python36-hourly-burst
-    aws events enable-rule --name coldstart-node810-hourly-burst
-    aws events enable-rule --name coldstart-java8-hourly-burst
-    aws events enable-rule --name coldstart-go-hourly-burst
-    aws events enable-rule --name coldstart-dotnet2-hourly-burst
+    aws events enable-rule --name coldstart-python36-dev-hourly-burst
+    aws events enable-rule --name coldstart-node810-dev-hourly-burst
+    aws events enable-rule --name coldstart-java8-dev-hourly-burst
+    aws events enable-rule --name coldstart-go-dev-hourly-burst
+    aws events enable-rule --name coldstart-dotnet2-dev-hourly-burst
   else
     echo "Using aws cli profile '$1'"
-    aws events enable-rule --name coldstart-python36-hourly-burst --profile $1 &
-    aws events enable-rule --name coldstart-node810-hourly-burst --profile $1 &
-    aws events enable-rule --name coldstart-java8-hourly-burst --profile $1 &
-    aws events enable-rule --name coldstart-go-hourly-burst --profile $1 &
-    aws events enable-rule --name coldstart-dotnet2-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-python36-dev-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-node810-dev-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-java8-dev-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-go-dev-hourly-burst --profile $1 &
+    aws events enable-rule --name coldstart-dotnet2-dev-hourly-burst --profile $1 &
 fi
