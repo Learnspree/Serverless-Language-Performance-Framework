33b8b3def6af5bc69048fecddd99ac1678dddc70
David Jackson <davidjacksonirl@gmail.com>
2019-11-28 21:01:20 +0000

    Added main deploy/remove scripts for dev and prod - just run one script now to deploy/remove entire SPF framework

---
+++ bin/spf-build-aws.sh
@@ -0,0 +1,21 @@
+echo "***** SPF: running full build script *****"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "***** SPF: running in $DIR *****"
+
+# build spf-api first (logger depends on this stack's output)
+echo "***** SPF: Build API *****"
+cd $DIR/spf-api
+./spf-build-api.sh
+
+# Build the test functions next (logger depends on this stack's log groups)
+echo "***** SPF: Build Test Functions *****"
+cd $DIR/aws-test
+./spf-build-aws-test.sh
+
+# Finally build the logger which takes test function log entries and delivers to the spf-api
+echo "***** SPF: Build Logger Handlers *****"
+cd $DIR/aws-common/nodejs-perf-logger
+./spf-build-aws-logger.sh
+
+echo "***** SPF: Finished *****"
---
+++ bin/spf-remove-aws.sh
@@ -0,0 +1,21 @@
+echo "***** SPF: running full remove script *****"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "***** SPF: running in $DIR *****"
+
+# Remove spf-api first (logger depends on this stack's output)
+echo "***** SPF: Remove API *****"
+cd $DIR/spf-api
+./spf-remove-api.sh
+
+# Remove the test functions next (logger depends on this stack's log groups)
+echo "***** SPF: Remove Test Functions *****"
+cd $DIR/aws-test
+./spf-remove-aws-test.sh
+
+# Finally remove the logger which takes test function log entries and delivers to the spf-api
+echo "***** SPF: Remove Logger Handlers *****"
+cd $DIR/aws-common/nodejs-perf-logger
+./spf-remove-aws-logger.sh
+
+echo "***** SPF: Finished Remove *****"
