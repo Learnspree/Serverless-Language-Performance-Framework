129513cd633045d568292e0bb4d917a9a6ee5828
David Jackson <davidjacksonirl@gmail.com>
2019-11-30 21:23:49 +0000

    cleanup of aws events (cloudwatch rules) scripts

--- bin/disable-all-rules.sh
+++ bin/disable-all-rules.sh
@@ -1,26 +1,40 @@
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied - using default aws cli profile"
-    aws events disable-rule --name warmstart-node810-dev-minute 
-    aws events disable-rule --name warmstart-java8-dev-minute 
-    aws events disable-rule --name warmstart-dotnet2-dev-minute
-    aws events disable-rule --name warmstart-python3-dev-minute
-    aws events disable-rule --name warmstart-golang-dev-minute
-    aws events disable-rule --name coldstart-python36-dev-hourly-burst
-    aws events disable-rule --name coldstart-node810-dev-hourly-burst
-    aws events disable-rule --name coldstart-java8-dev-hourly-burst
-    aws events disable-rule --name coldstart-go-dev-hourly-burst
-    aws events disable-rule --name coldstart-dotnet2-dev-hourly-burst
-  else
-    echo "Using aws cli profile '$1'"
-    aws events disable-rule --name warmstart-node810-dev-minute --profile $1 &
-    aws events disable-rule --name warmstart-java8-dev-minute --profile $1 &
-    aws events disable-rule --name warmstart-dotnet2-dev-minute --profile $1 &
-    aws events disable-rule --name warmstart-python3-dev-minute --profile $1 &
-    aws events disable-rule --name warmstart-golang-dev-minute --profile $1 &
-    aws events disable-rule --name coldstart-python36-dev-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-node810-dev-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-java8-dev-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-go-dev-hourly-burst --profile $1 &
-    aws events disable-rule --name coldstart-dotnet2-dev-hourly-burst --profile $1 &
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -e environment"
+   echo -e "\t-e target environment (dev or prod)"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "e:" opt
+do
+   case "$opt" in
+      e ) environment="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$environment" ] 
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
 fi
+
+if [[ $environment != "dev" ]] && [[ $environment != "prod" ]]; then
+    echo "Some or all of the parameters are incorrect";
+    helpFunction
+fi
+
+aws events disable-rule --name warmstart-node810-$environment-minute 
+aws events disable-rule --name warmstart-java8-$environment-minute 
+aws events disable-rule --name warmstart-dotnet21-$environment-minute 
+aws events disable-rule --name warmstart-python3-$environment-minute 
+aws events disable-rule --name warmstart-golang-$environment-minute 
+aws events disable-rule --name coldstart-python36-$environment-hourly-burst 
+aws events disable-rule --name coldstart-node810-$environment-hourly-burst 
+aws events disable-rule --name coldstart-java8-$environment-hourly-burst 
+aws events disable-rule --name coldstart-go-$environment-hourly-burst 
+aws events disable-rule --name coldstart-dotnet21-$environment-hourly-burst 
