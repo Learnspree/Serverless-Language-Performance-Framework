8e5f669f24b0860940046fdaad0dbecd50d4966c
David Jackson <davidjacksonirl@gmail.com>
2020-02-10 15:46:56 +0000

    Adding test phase to aws-test functions to ensure they all run OK after deployment

--- aws-test/spf-build-aws-test.sh
+++ aws-test/spf-build-aws-test.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
 echo "***** SPF: running build script *****"
+set -e
+
+while getopts "t" opt
+do
+   case "$opt" in
+      t ) testing="test" ;;
+   esac
+done
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "***** SPF: running in $DIR *****"
@@ -16,6 +26,37 @@ cd $DIR/aws-service-go
 make
 
 echo "***** SPF: finished build stage *****"
+if [ -z $testing ]
+then
+    echo "***** SPF: skipping testing stage *****"
+else
+    echo "***** SPF: running testing stage *****"
+    echo "***** SPF: testing each target function runs ok.... *****"
+    cd $DIR
+    sls invoke -f aws-warm-empty-dotnet21 --stage dev
+    sls invoke -f aws-warm-256-empty-dotnet21 --stage dev
+    sls invoke -f aws-warm-empty-go --stage dev
+    sls invoke -f aws-warm-256-empty-go --stage dev
+    sls invoke -f aws-warm-empty-java8 --stage dev
+    sls invoke -f aws-warm-256-empty-java8 --stage dev
+    sls invoke -f aws-warm-empty-nodejs12x --stage dev
+    sls invoke -f aws-warm-256-empty-nodejs12x --stage dev
+    sls invoke -f aws-warm-empty-python36 --stage dev
+    sls invoke -f aws-warm-256-empty-python36 --stage dev
+
+    sls invoke -f aws-cold-empty-dotnet21 --stage dev
+    sls invoke -f aws-cold-256-empty-dotnet21 --stage dev
+    sls invoke -f aws-cold-empty-go --stage dev
+    sls invoke -f aws-cold-256-empty-go --stage dev
+    sls invoke -f aws-cold-empty-java8 --stage dev
+    sls invoke -f aws-cold-256-empty-java8 --stage dev
+    sls invoke -f aws-cold-empty-nodejs12x --stage dev
+    sls invoke -f aws-cold-256-empty-nodejs12x --stage dev
+    sls invoke -f aws-cold-empty-python36 --stage dev
+    sls invoke -f aws-cold-256-empty-python36 --stage dev
+
+    echo "***** SPF: finished testing stage *****"
+fi
 
 echo "***** SPF: running sls deploy stage *****"
 
