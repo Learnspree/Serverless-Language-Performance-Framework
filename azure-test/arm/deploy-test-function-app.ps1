56883c8895942437860c22161e646de1a642d5d6
David Jackson <davidjacksonirl@gmail.com>
2020-03-12 10:30:42 +0000

    remove need to specify lowercase region in azure powershell script

--- azure-test/arm/deploy-test-function-app.ps1
+++ azure-test/arm/deploy-test-function-app.ps1
@@ -1,16 +1,19 @@
 # Example usage from powershell:
-# ./deploy-test-function-app.ps1 -runtime "python" -region "East US" -regionLowercase "east-us"
+# ./deploy-test-function-app.ps1 -runtime "python" -region "East US"
 
 # Connect to the Azure Account
 ## Run 'pwsh' from macos terminal to start powershell
 ## Run 'Connect-AzAccount' before running this script for now (will setup a service principal in future)
 
 # Note - param() must be the first statement in the script
-param([string]$runtime="node",[string]$region="East US",[string]$regionLowercase="east-us") 
+param([string]$runtime="node",[string]$region="East US") 
 
 # Register Resource Providers if they're not already registered
 Register-AzResourceProvider -ProviderNamespace "microsoft.web"
 Register-AzResourceProvider -ProviderNamespace "microsoft.storage"
+
+# create lowercase version of region with hyphens instead of spaces to help with resource naming
+$regionLowercase = "${region}".ToLower().Replace(' ', '-')
 
 # Create a resource group for the function app
 New-AzResourceGroup -Name "spf-azure-test-${runtime}-${regionLowercase}-rg" -Location "${region}" -Force
