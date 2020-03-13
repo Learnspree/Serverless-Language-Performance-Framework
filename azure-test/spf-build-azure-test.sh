df0d8cccebcae5aa1e9d83b7b069cd2bee8a575a
David Jackson <davidjacksonirl@gmail.com>
2020-03-13 20:19:22 +0000

    Add login with service principal to azure deploy script

--- azure-test/spf-build-azure-test.sh
+++ azure-test/spf-build-azure-test.sh
@@ -3,22 +3,23 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -r region [-l runtime]"
-   echo -e "\t-r [target Azure region].... (e.g. 'East US') [-l language-runtime]"
+   echo "Usage: $0 -r region -p deploy_password [-l runtime]"
+   echo -e "\t-r [target Azure region].... (e.g. 'East US') -p deploy_password [-l language-runtime]"
    exit 1 # Exit script after printing help
 }
 
-while getopts "l:r:" opt
+while getopts "l:r:p:" opt
 do
    case "$opt" in
       r ) region="$OPTARG" ;;
       l ) languageRuntime="$OPTARG" ;;
+      p ) servicePrincipalPassword="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$region" ] 
+if [ -z "$region" ] || [ -z "$servicePrincipalPassword" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -40,6 +41,11 @@ echo "***** SPF: running build script for Azure Function app for region '$region
 echo ""
 
 cd $DIR/arm
+
+# Login with service principal 
+echo "***** SPF: Logging in with SPFDeploymentServicePrincipal *****"
+echo ""
+pwsh -f login-with-service-principal.ps1 -servicePrincipalPass $servicePrincipalPassword
 
 # Deploy just the given runtime if provided, otherwise deploy all
 if [ -n "$languageRuntime" ]
