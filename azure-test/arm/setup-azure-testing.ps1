de30e7f9f68b6833acf38e72c2c8ea3e9493001d
David Jackson <davidjacksonirl@gmail.com>
2020-03-14 15:43:53 +0000

    Added script for initial setup of azure service principal for automated deployments. Updated README

---
+++ azure-test/arm/setup-azure-testing.ps1
@@ -0,0 +1,21 @@
+# Pre-requisite: Run 'Connect-AzAccount' from powershell to login to your subscription
+# Example usage: pwsh setup-azure-testing.ps1 -servicePrincipalPass "<password>"
+
+# Note - param() must be the first statement in the script
+param([string]$servicePrincipalPass) 
+
+# Import the PSADPasswordCredential object
+Import-Module Az.Resources 
+
+# create credentials for service principal
+$credentials = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property @{ StartDate=Get-Date; EndDate=Get-Date -Year 2024; Password=$servicePrincipalPass} 
+
+# create service principal
+$principalDisplayName = "SPFDeploymentServicePrincipal"
+New-AzAdServicePrincipal -DisplayName $principalDisplayName -PasswordCredential $credentials
+
+# create role assignment
+$subId = (Get-AzSubscription).Id
+$principalObjectId = (Get-AzADServicePrincipal -DisplayName $principalDisplayName).Id
+New-AzRoleAssignment -ObjectId $principalObjectId -RoleDefinitionName "Contributor" -Scope "/subscriptions/$subId"
+
