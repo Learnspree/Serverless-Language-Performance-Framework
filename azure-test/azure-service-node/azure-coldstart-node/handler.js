a04b382983fc63e233b48d3db74c6ea2bfdcc01d
David Jackson <davidjacksonirl@gmail.com>
2020-03-23 14:34:36 +0000

    Added basic detection of cold vs warm start in node azure function

--- azure-test/azure-service-node/azure-coldstart-node/handler.js
+++ azure-test/azure-service-node/azure-coldstart-node/handler.js
@@ -2,8 +2,23 @@
 
 /* eslint-disable no-param-reassign */
 
+function invocationCountIncrement(context){
+    invocationCountIncrement.counter++;
+    context.log("counter:" + invocationCountIncrement.counter);
+
+    if (invocationCountIncrement.counter > 1) {
+      context.log("warm start");
+    }
+    else {
+      context.log("cold start");
+    }
+}
+
+invocationCountIncrement.counter = 0;
+
 module.exports.empty = function (context, nodeJSEmptyFunctionTimer) {
 
+  var invocationCountIncrementer = new invocationCountIncrement(context);  
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: 'Empty azure node function executed successfully!',
