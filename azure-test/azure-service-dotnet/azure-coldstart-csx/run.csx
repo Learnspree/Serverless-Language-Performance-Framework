66cd281e8225dcbe77958a5d6a05cdebd0fc5c8c
David Jackson <davidjacksonirl@gmail.com>
2020-03-23 15:50:04 +0000

    Refactored azure test function packages to use a single shared source file rather than duplicated same code

--- azure-test/azure-service-dotnet/azure-coldstart-csx/run.csx
+++
@@ -1,8 +0,0 @@
-using System;
-
-static int ExecutionCounter = 0;
-public static void Run(TimerInfo myTimer, TraceWriter log)
-{
-    ExecutionCounter++;
-    log.Info(((ExecutionCounter > 1) ? "Warm" : "Cold") + " Start");
-}
--- azure-test/azure-service-node/shared-azure-node/handler.js
+++ azure-test/azure-service-node/shared-azure-node/handler.js
@@ -4,12 +4,17 @@
 
 function invocationCountIncrement(context){
     invocationCountIncrement.counter++;
-    context.log((invocationCountIncrement.counter > 1) ? "warm" : "cold" + " start");
+    context.log(((invocationCountIncrement.counter > 1) ? "warm" : "cold") + " start");
 }
 
 invocationCountIncrement.counter = 0;
 
 module.exports.empty = function (context, nodeJSEmptyFunctionTimer) {
+
+  // todo - global variables shared across azure functions (e.g. cold and warm functions) so doesn't work for cold/warm detection
+  // instead - use env variables
+  // Read: let value = process.env.ENV_VARIABLE
+  // Write: process.env.ENV_VARIABLE = value
 
   var invocationCountIncrementer = new invocationCountIncrement(context);  
   context.res = {
