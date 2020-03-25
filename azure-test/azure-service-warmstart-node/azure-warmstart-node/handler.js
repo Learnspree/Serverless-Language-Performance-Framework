5994cd7cf2c16c3a806339560900391e47c67efd
David Jackson <davidjacksonirl@gmail.com>
2020-03-25 11:57:56 +0000

    Successfully logging trace message via TrackTrace() call in node test function ... BUT... startup times too long due to required 'applicationInsights' dependency loading affects empty function timings

--- azure-test/azure-service-warmstart-node/azure-warmstart-node/handler.js
+++ azure-test/azure-service-warmstart-node/azure-warmstart-node/handler.js
@@ -2,15 +2,23 @@
 
 /* eslint-disable no-param-reassign */
 
+var appInsights = require("applicationinsights");
+appInsights.setup();
+var client = new appInsights.TelemetryClient();
+
 module.exports.empty = function (context, nodeJSEmptyFunctionTimer) {
 
   // Using env variables to detect cold vs warm start
-  context.log("Context: " + JSON.stringify(context));
-
   let currentInvocationCount = parseInt(("INVOCATION_COUNT" in process.env) ? process.env.INVOCATION_COUNT : "0");
   context.log("currentInvocationCount: " + currentInvocationCount);
-  context.log(((currentInvocationCount > 0) ? "warm" : "cold") + " start");
+  let functionState = ((currentInvocationCount > 0) ? "warm" : "cold");
   process.env.INVOCATION_COUNT = currentInvocationCount + 1;
+  context.log( functionState + " start");
+  client.trackTrace({
+    message: 'cold start state', 
+    severity: appInsights.Contracts.SeverityLevel.Warning,
+    properties: { "state" : functionState }
+  });
 
   context.res = {
     // status: 200, /* Defaults to 200 */
