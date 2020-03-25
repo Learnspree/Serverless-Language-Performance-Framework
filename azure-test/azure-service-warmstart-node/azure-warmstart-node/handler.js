0a64f158ca997e891cea153f6d87e0c00e4c4c38
David Jackson <davidjacksonirl@gmail.com>
2020-03-25 13:53:38 +0000

    Removed applicationinsights dependency from azure node test functions - logging trace message for cold/warm via context.log instead

--- azure-test/azure-service-warmstart-node/azure-warmstart-node/handler.js
+++ azure-test/azure-service-warmstart-node/azure-warmstart-node/handler.js
@@ -1,24 +1,16 @@
 'use strict';
 
 /* eslint-disable no-param-reassign */
-
-var appInsights = require("applicationinsights");
-appInsights.setup();
-var client = new appInsights.TelemetryClient();
-
 module.exports.empty = function (context, nodeJSEmptyFunctionTimer) {
 
   // Using env variables to detect cold vs warm start
   let currentInvocationCount = parseInt(("INVOCATION_COUNT" in process.env) ? process.env.INVOCATION_COUNT : "0");
-  context.log("currentInvocationCount: " + currentInvocationCount);
   let functionState = ((currentInvocationCount > 0) ? "warm" : "cold");
   process.env.INVOCATION_COUNT = currentInvocationCount + 1;
-  context.log( functionState + " start");
-  client.trackTrace({
-    message: 'cold start state', 
-    severity: appInsights.Contracts.SeverityLevel.Warning,
-    properties: { "state" : functionState }
-  });
+
+  // write to log so app-insights can pick it up and trigger a match with the default logged function 'request' metrics picked up by the logger
+  // this will write associated trace properties to app insights automatically including crucial "hostId" value
+  context.log.error( "SPF Function State: " + functionState);
 
   context.res = {
     // status: 200, /* Defaults to 200 */
