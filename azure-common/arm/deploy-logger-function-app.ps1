505868a73dee361aa9e9981f39641c38115c5288
David Jackson <davidjacksonirl@gmail.com>
2020-03-28 21:55:54 +0000

    adding spf url driven off environment (dev/prod) to logger in azure - needs testing

--- azure-common/arm/deploy-logger-function-app.ps1
+++ azure-common/arm/deploy-logger-function-app.ps1
@@ -1,33 +1,31 @@
 # Example usage:
-# [pwsh] ./deploy-logger-function-app.ps1 -region "East US"
+# [pwsh] ./deploy-logger-function-app.ps1 -region "East US" [-environment "dev"]
 # Note - param() must be the first statement in the script
 param(
-[Parameter(Mandatory=$True)]
-    [string]$region
-) 
+    [Parameter(Mandatory=$True)]
+    [string]$region,
 
-# Register Resource Providers if they're not already registered 
-# Commenting out - NOT NEEDED HERE??
-#Register-AzResourceProvider -ProviderNamespace "microsoft.web"
-#Register-AzResourceProvider -ProviderNamespace "microsoft.storage"
+    [Parameter(Mandatory=$False)]
+    [string]$environment = "dev"
+) 
 
 # create lowercase version of region with hyphens instead of spaces to help with resource naming
 $regionLowercase = "${region}".ToLower().Replace(' ', '-')
-$rgName = "spf-azure-logger-${regionLowercase}-rg"
+$rgName = "spf-azure-logger-${regionLowercase}-${environment}-rg"
 
 # Create a resource group for the function app
 New-AzResourceGroup -Name $rgName -Location "${region}" -Force
 
 # Create the parameters for the file
-$TemplateParams = @{"appName" = "spf-azure-logger-${regionLowercase}"}
+$appName = "spf-azure-logger-${regionLowercase}-${environment}"
+$TemplateParams = @{"appName" = "${appName}"; "spfenvironment" = "${environment}"}
 
 # Deploy the function app ARM template
 New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile "azure-logger-function-deploy.json" -TemplateParameterObject $TemplateParams -Verbose -Force
 
 # Zip the logger function package
-$zippath = "./azure-logger-$regionLowercase.zip"
+$zippath = "./azure-logger-$regionLowercase-${environment}.zip"
 Compress-Archive -Path ../azure-logger/* -DestinationPath $zippath -Force
 
 # Deploy the function
-$appName = "spf-azure-logger-${regionLowercase}"
 Publish-AzWebapp -ResourceGroupName $rgName -Name $appName -ArchivePath $zippath -Force
